536fa14fe7799ce62fef87ba878a72d59b42b356
Acep Saepudin <acep.saepudin@finaccel.co>
2020-09-01 10:40:12 +0700

    remove secret key

--- pomang/pomang/settings.py
+++ pomang/pomang/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sr$**ex4(wjz09ocz-befoc_$yo83+babq8egsv4lfop@54w@e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
